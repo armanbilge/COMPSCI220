2186e42b8f303ec340bbefbfe77209ada3dd55e0
Arman Bilge <armanbilge@gmail.com>
2015-04-10 16:57:09 +1200

    Add assignment 2 files

---
+++ A2/a2_quick.java
@@ -0,0 +1,147 @@
+import java.io.*;
+import java.util.*;
+
+public class a2_quick {
+   private static int[] unsorted;  // Integers to sort
+
+   private static int get_unsorted()
+         throws IOException {
+      Scanner sysin = new Scanner(System.in);
+      int n = sysin.nextInt();
+      unsorted = new int[n];
+      for (int j = 0; j != n; j += 1)  // Read the integers into unsorted
+         unsorted[j] = sysin.nextInt();
+      sysin.close();
+      return n;
+   }
+
+   private static void get_values(int[] a, String fn, int n) {
+      // Copy values from unsorted to a
+      System.arraycopy(unsorted,0, a,0, n);
+   }
+
+
+   private static void swap(int[]a, int i, int j) {
+      int tmp = a[i];  a[i] = a[j];  a[j] = tmp;
+   }
+
+   private static void percolateDown(int []a, int i, int size) {
+      int child;
+      int parent = i + 1;
+      for (child = parent*2; child < size; child = parent*2) {
+         if (a[parent-1] < a[child-1] || a[ parent-1] < a[child] ) {
+            if (a[child-1] < a[child]) {
+               swap(a, parent-1, child);
+               parent = child+1;
+            } else {
+	       swap(a, parent-1, child-1);
+               parent = child;
+            }
+         } else break;
+      }
+      if (child == size && a[parent-1] < a[child-1])
+         swap(a, parent-1, child-1);
+   }
+
+   public static void heapSort(int[] a) {
+      for (int i = a.length/2 - 1; i >= 0; i--)
+         percolateDown(a, i, a.length);
+      for(int i = a.length-1; i >= 1; i--) {
+         swap(a, 0, i);
+         percolateDown(a, 0, i);
+      }
+   }
+
+
+   private static void quicksort(int[] a, int left, int right, int which) {
+      int px;
+      if (which == 1) px = p_index1(a, left, right);
+      else px = p_index2(a, left, right);
+
+      if (px < left || px > right) {
+	  System.out.printf("p_index failed");
+	  System.exit(111);
+      }
+
+      int pivot = a[px];
+      int lx = left, rx = right;
+      while (lx <= rx) {
+	 while (a[lx] < pivot) lx += 1;
+         while (a[rx] > pivot) rx -= 1;
+         if (lx <= rx) {
+    	    swap(a, lx, rx);
+     	    lx += 1;  rx -= 1;
+	 }
+      }
+      
+      if (left < lx-1)
+      	  quicksort(a, left, lx-1, which);
+      if (right > lx)
+	  try {
+	      quicksort(a, lx, right, which);
+	  } catch (StackOverflowError e) {
+	     System.out.printf(
+	        "StackOverflow:  left=%d, lx=%d, right=%d\n",
+		left, lx, right);
+             System.out.flush();
+	     return;
+	  }
+   }
+
+   private static int p_index1(int[] a, int left, int right) {
+      return left+(right-left)/500;
+   }
+      
+   private static int p_index2(int[] a, int left, int right) {
+      return left+(right-left)/500;
+   }
+
+   public static void quickSort(int[] a, int which) {
+      quicksort(a, 0, a.length-1, which);
+   }
+
+   private static void test_algorithms(int a[], int n, String fn)
+         throws IOException {
+      long start;
+      double TimeInSec_h = 0, TimeInSec_q1 = 0, TimeInSec_q2 = 0;
+      String speedup;
+
+      get_values(a, fn, n);
+      start = System.nanoTime();
+      heapSort(a);
+      TimeInSec_h = (System.nanoTime()-start) * 1.0e-9;
+
+      get_values(a, fn, n);
+      start = System.nanoTime();
+      quickSort(a, 1);
+      TimeInSec_q1 = (System.nanoTime()-start) * 1.0e-9;
+
+      get_values(a, fn, n);
+      start = System.nanoTime();
+      quickSort(a, 2);
+      TimeInSec_q2 = (System.nanoTime()-start) * 1.0e-9;
+
+      speedup = String.format("%.1f", TimeInSec_q1/TimeInSec_q2);
+      System.out.printf("%8d   %9.6f   %9.6f  %9.6f     %s\n", n,
+         TimeInSec_h, TimeInSec_q1, TimeInSec_q2, speedup);
+   }
+
+   public static void main(String[] args) throws IOException {
+      String fn = "a2_in.txt";
+      int n = get_unsorted();
+      System.out.printf("%d integers read\n", n);
+
+      int nv[] = new int[6];
+      int m = n, d = 1;
+      for (int j = 5; j >= 0; j -= 1) {
+         nv[j] = m/d;  d *= 2;
+      }
+
+      int a[] = new int[n];
+      System.out.printf(
+	 "#     n       heap        quick1     quick2   speedup\n");
+      for (int x = 0; x != nv.length; x += 1) {
+         test_algorithms(a, nv[x], fn);
+      }
+   }
+}
