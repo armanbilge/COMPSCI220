dd55f0b0085b8bbd4bd210f3ec6fffa29a4e6329
Arman Bilge <armanbilge@gmail.com>
2015-04-30 09:39:03 +1200

    Update to latest files

--- A2/a2_quick.java
+++ A2/a2_quick.java
@@ -4,8 +4,31 @@ import java.util.*;
 public class a2_quick {
    private static int[] unsorted;  // Integers to sort
 
-   private static int get_unsorted()
-         throws IOException {
+   /* This version of get_unsorted() reads the integers directly
+      from file a2_in.txt.  Use it if you're developing your version
+      of a2_quick using DrJava on Windows.
+
+      >> Don't forget to switch back to the other version (which
+         reads from stdin) before you submit your program to the
+         www.cs.auckland.ac.nz/automated-marker <<<
+    
+   private static int get_unsorted() {
+      File inf = new File("a2_in.txt");
+      int n = 0;
+      Scanner scanner;
+      try {
+         scanner = new Scanner(inf);
+         n = scanner.nextInt();
+         unsorted = new int[n];
+         for (int j = 0; j != n; j += 1)  // Read the integers into unsorted
+            unsorted[j] = scanner.nextInt();
+      } catch (FileNotFoundException e) {
+	 System.out.printf("Couldn't find file a2_in.txt\n");    
+      }
+      return n;
+    } */
+
+    private static int get_unsorted() {
       Scanner sysin = new Scanner(System.in);
       int n = sysin.nextInt();
       unsorted = new int[n];
@@ -13,9 +36,9 @@ public class a2_quick {
          unsorted[j] = sysin.nextInt();
       sysin.close();
       return n;
-   }
+      }
 
-   private static void get_values(int[] a, String fn, int n) {
+    private static void get_values(int[] a, int n) {
       // Copy values from unsorted to a
       System.arraycopy(unsorted,0, a,0, n);
    }
@@ -55,8 +78,8 @@ public class a2_quick {
 
    private static void quicksort(int[] a, int left, int right, int which) {
       int px;
-      if (which == 1) px = p_index1(a, left, right);
-      else px = p_index2(a, left, right);
+      if (which == 1) px = p_index1(left, right);
+      else px = p_index2(left, right);
 
       if (px < left || px > right) {
 	  System.out.printf("p_index failed");
@@ -88,46 +111,44 @@ public class a2_quick {
 	  }
    }
 
-   private static int p_index1(int[] a, int left, int right) {
-      return left+(right-left)/500;
+   private static int p_index1(int left, int right) {
+      return left+(right-left)/200;
    }
-      
-   private static int p_index2(int[] a, int left, int right) {
-      return left+(right-left)/500;
+
+   private static int p_index2(int left, int right) {
+      return left+(right-left)/200;
    }
 
    public static void quickSort(int[] a, int which) {
       quicksort(a, 0, a.length-1, which);
    }
 
-   private static void test_algorithms(int a[], int n, String fn)
-         throws IOException {
+   private static void test_algorithms(int a[], int n) {
       long start;
       double TimeInSec_h = 0, TimeInSec_q1 = 0, TimeInSec_q2 = 0;
       String speedup;
 
-      get_values(a, fn, n);
+      get_values(a, n);
       start = System.nanoTime();
       heapSort(a);
       TimeInSec_h = (System.nanoTime()-start) * 1.0e-9;
 
-      get_values(a, fn, n);
+      get_values(a, n);
       start = System.nanoTime();
       quickSort(a, 1);
       TimeInSec_q1 = (System.nanoTime()-start) * 1.0e-9;
 
-      get_values(a, fn, n);
+      get_values(a, n);
       start = System.nanoTime();
       quickSort(a, 2);
       TimeInSec_q2 = (System.nanoTime()-start) * 1.0e-9;
-
+      
       speedup = String.format("%.1f", TimeInSec_q1/TimeInSec_q2);
       System.out.printf("%8d   %9.6f   %9.6f  %9.6f     %s\n", n,
-         TimeInSec_h, TimeInSec_q1, TimeInSec_q2, speedup);
+      TimeInSec_h, TimeInSec_q1, TimeInSec_q2, speedup);  /* */
    }
 
-   public static void main(String[] args) throws IOException {
-      String fn = "a2_in.txt";
+   public static void main(String[] args) {
       int n = get_unsorted();
       System.out.printf("%d integers read\n", n);
 
@@ -141,7 +162,7 @@ public class a2_quick {
       System.out.printf(
 	 "#     n       heap        quick1     quick2   speedup\n");
       for (int x = 0; x != nv.length; x += 1) {
-         test_algorithms(a, nv[x], fn);
+         test_algorithms(a, nv[x]);
       }
    }
 }
